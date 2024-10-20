3b6aa3f953e9917c332e42b41bc7d337501b2486
Moataz <abdorashwan536@gmail.com>
2024-10-21 00:01:04 +0300

    Solved The Second Problem

---
+++ codeforces/Sheet_Assit/Sheet_1/(B. Basic Data Types).py
@@ -0,0 +1,13 @@
+data = input().split()
+
+int_Value = int(data[0])
+long_Value = int(data[1])
+char_Value = str(data[2])
+float_Value = float(data[3])
+double_Value = float(data[4])
+
+print(int_Value)
+print(long_Value)
+print(char_Value)
+print(float_Value)
+print(double_Value)
