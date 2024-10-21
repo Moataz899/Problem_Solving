61257e8e554e950cfac8738a75ba5742c569eea6
Moataz <abdorashwan536@gmail.com>
2024-10-21 22:42:55 +0300

    Solved The Thired Problem

---
+++ codeforces/Sheet_Assit/Sheet_1/(C. Simple Calculator).py
@@ -0,0 +1,10 @@
+data = input().split()
+
+X = int(data[0])
+Y = int(data[1])
+
+if X >= 1 and X >=1 :
+  print(X ,"+", Y ,"=",(X+Y))
+  print(X ,"*", Y ,"=",(X*Y))
+  print(X ,"-", Y ,"=",(X-Y))
+  
